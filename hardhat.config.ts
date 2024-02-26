f3270d1183c84e1a10f8a2a0ba283733459ee154
Jeremiah Samuel <livinalt@gmail.com>
2024-02-26 18:26:35 +0100

    commit

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+
+
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_MAINNET_API_KEY_URL,
+      }
+    },
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+    mumbai: {
+      url: ALCHEMY_MUMBAI_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    }
+  },
+  lockGasLimit: 200000000000,
+  gasPrice: 10000000000,
+};
