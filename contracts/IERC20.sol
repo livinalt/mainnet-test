c2127fbb64b18d21b9096cd6f59cd01e6cab0ed8
Jeremiah Samuel <livinalt@gmail.com>
2024-02-26 21:44:07 +0100

    update

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -4,4 +4,8 @@ pragma solidity 0.8.24;
 interface IERC20{
     function approve(address _spender, uint _value) external;
     function balanceOf(address who) external view returns(uint256 balance);
+    function name() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
